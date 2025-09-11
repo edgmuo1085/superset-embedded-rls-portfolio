c9a309ed45332c149debb1977e77da58da0ebd8c
Edgar Munoz Q <edgmuo1085@gmail.com>
2025-09-11 11:45:49 -0500

    se agrega mas config para variables de entorno

--- 2025/readme.sh
+++ 2025/readme.sh
@@ -1,5 +1,6 @@
 ### Ejecutar las variables de entorno para el sistema
-export ALLOWED_EMBEDDED_DOMAINS=http://localhost:3001
-export METADATA_DB_URI=postgresql+psycopg2://superset:superset@db:5432/superset
-export SUPERSET_SECRET_KEY=1
-export SUPERSET_JWT_SECRET=1
+
+export ALLOWED_EMBEDDED_DOMAINS="http://localhost:3001" 
+export METADATA_DB_URI="postgresql+psycopg2://superset:superset@db:5432/superset" 
+export SUPERSET_SECRET_KEY="1" 
+export SUPERSET_JWT_SECRET="1" 
