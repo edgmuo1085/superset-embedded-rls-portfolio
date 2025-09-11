16531c71a9d636bc3cb5414b9dcdaf6b7942d332
Edgar Munoz Q <edgmuo1085@gmail.com>
2025-09-11 13:22:08 -0500

    se ajusta dominios

--- 2025/readme.sh
+++ 2025/readme.sh
@@ -1,6 +1,5 @@
 ### Ejecutar las variables de entorno para el sistema
 
-export SUPERSET_DOMAINS_ALLOW="http://localhost:3001|http://localhost:3002"
 export METADATA_DB_URI="postgresql+psycopg2://superset:superset@db:5432/superset" 
 export SUPERSET_SECRET_KEY="1" 
 export SUPERSET_JWT_SECRET="1" 
--- 2025/superset_config.py
+++ 2025/superset_config.py
@@ -7,9 +7,7 @@ logger = logging.getLogger(__name__)
 
 logger.info("⚡ Config personalizada cargada ⚡")
 
-
 ### IMPRIMIR VARIABLES DE ENTORNO   
-logger.info("SUPERSET_DOMAINS_ALLOW: %s", os.environ.get("SUPERSET_DOMAINS_ALLOW", "http://localhost:3002"))
 logger.info("SUPERSET_SECRET_KEY: %s",      os.environ.get("SUPERSET_SECRET_KEY", "default_inseguro_cambiar"))
 logger.info("SUPERSET_JWT_SECRET: %s",      os.environ.get("SUPERSET_JWT_SECRET", "jwt_inseguro_cambiar"))
 logger.info("SUPERSET_SQLALCHEMY_DATABASE_URI: %s", os.environ.get("SUPERSET_SQLALCHEMY_DATABASE_URI", "postgresql+psycopg2://superset:superset@db:5432/superset"))
@@ -20,16 +18,11 @@ logger.info("SUPERSET_SQLALCHEMY_DATABASE_URI: %s", os.environ.get("SUPERSET_SQL
 # ==============================
 SECRET_KEY = os.getenv("SUPERSET_SECRET_KEY", "default_inseguro_cambiar")
 JWT_SECRET = os.getenv("SUPERSET_JWT_SECRET", "jwt_inseguro_cambiar")
-SUPERSET_DOMAINS_ALLOW_TMP = os.getenv("SUPERSET_DOMAINS_ALLOW", "jwt_inseguro_cambiar")
-logger.info("SUPERSET_DOMAINS_ALLOW_TMP: %s", SUPERSET_DOMAINS_ALLOW_TMP)
 
 # --- Settings for Embedding & Guest Tokens ---
 # ENABLE_EMBEDDED_SUPERSET is controlled by env var
+ALLOWED_EMBEDDED_DOMAINS = ["http://localhost:8000","http://localhost:3001","http://localhost:3002"]
 
-SUPERSET_DOMAINS_ALLOW = os.environ.get("SUPERSET_DOMAINS_ALLOW", "").split("|")
-SUPERSET_DOMAINS_ALLOW = [domain.strip() for domain in SUPERSET_DOMAINS_ALLOW if domain.strip()]
-if not SUPERSET_DOMAINS_ALLOW:
-    SUPERSET_DOMAINS_ALLOW = ["http://localhost:8000","http://localhost:3001","http://localhost:3002"]
 
 # --- Session Cookie Settings ---
 SESSION_COOKIE_SAMESITE = None
@@ -63,7 +56,7 @@ CORS_OPTIONS = {
   'supports_credentials': True,
   'allow_headers': ['*'],
   'resources':['*'],
-  'origins': SUPERSET_DOMAINS_ALLOW
+  'origins': ALLOWED_EMBEDDED_DOMAINS
 }
 
 # ==============================
@@ -77,8 +70,8 @@ GUEST_ROLE_NAME = "Gamma"
 EMBEDDED_SUPERSET = {
     "guest_token_jwt_secret": JWT_SECRET,
     "guest_token_jwt_exp_seconds": int(timedelta(hours=1).total_seconds()),
-    "allowed_domains": SUPERSET_DOMAINS_ALLOW,
+    "allowed_domains": ALLOWED_EMBEDDED_DOMAINS,
 }
 
-logger.info("SUPERSET_DOMAINS_ALLOW: %s", SUPERSET_DOMAINS_ALLOW)
+logger.info("ALLOWED_EMBEDDED_DOMAINS: %s", ALLOWED_EMBEDDED_DOMAINS.split(","))
 logger.info("✅ Embedding habilitado con rol %s", GUEST_ROLE_NAME)
